fe784395a277a25c048c71fbda991c1e53832028
Mingyu Xia <myhsia@outlook.com>
2025-03-20 17:24:47 +0800

    Version 4.2B released. - Updated the manual.

--- build.lua
+++ build.lua
@@ -7,20 +7,20 @@ ctanzip      = module
 
 uploadconfig = {
   pkg          = "notebeamer",
-  version      = "v4.2A 2025-03-18",
+  version      = "v4.2B 2025-03-20",
   author       = "Mingyu Xia",
   uploader     = "Mingyu Xia",
   email        = "myhsia@outlook.com",
   summary      = "Package for printing presentations on notepaper",
   description  = [[
     The notebeamer package provides an easy way to input slides on note pages
-    quickly, developed by expl3 based on TikZ and l3graphics.
+    quickly, developed by expl3 based on TikZ.
   ]],
   license      = "lppl1.3c",  
   ctanPath     = "/macros/latex/contrib/notebeamer",
   announcement = [[
-    Version 4.2A released.
-    - Ensure compatibility with the recent integrate of l3graphics to l3kernel.
+    Version 4.2B released.
+    - Updated the manual.
   ]],
   home         = "https://github.com/myhsia/notebeamer",
   bugtracker   = "https://github.com/myhsia/notebeamer/issues",
